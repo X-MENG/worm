83e68a8bc1624835a37c6f3ac19cde4a95dd81d7
meng <59571513@qq.com>
2017-08-18 14:35:53 +0800

    finish converter

--- converter.py
+++ converter.py
@@ -1,10 +1,13 @@
 import pygame
 import json
+import os.path
 from glob import glob
 
 class Converter:
 	def __init__(self, unit_root):
 		self.unit_list = glob(unit_root + "/" + "*.png");
+
+		unit_dict = {};
 
 		for unit_name in self.unit_list:
 			name = unit_name.split("\\")[1];
@@ -14,32 +17,88 @@ class Converter:
 
 			cur_img = pygame.image.load(p);
 
-			for i in range(0, 128):
-				for j in range(0, 128):
-					clr = cur_img.get_at((i, j));
+			for j in range(0, 128):
+				for i in range(0, 128):
+					clr = cur_img.get_at((i, j));				
 					if clr == (255, 0, 0, 255):
-						print((i, j));
+						#print("anchor: (%s, %s)" % (i, j));
+						n = self.get_neighbour_pixel(cur_img, i, j, (0, 255, 0, 255));
+						print("cur name = " + name);
+						if name in unit_dict.keys():
+							unit_dict[name].append([(i, j), n]);
+						else:
+							unit_dict[name] = [];
+							unit_dict[name].append([(i, j), n]);
 
+		#self.width  = 800;
+		#self.height = 600;
 
-			self.width  = 800;
-			self.height = 600;
-			self.screen = pygame.display.set_mode((self.width, self.height), 0, 32);
-			pygame.display.set_caption("worm");
+		#self.screen = pygame.display.set_mode((self.width, self.height), 0, 32);
+		#pygame.display.set_caption("worm");
 
-		#self.test_img = pygame.image.load(unit_root + "/" + "mask" + "/" + "mask_diamond.png");
+		json_dict = json.dumps(unit_dict);
+
+		print(json_dict);
+
+		wf = open("config/unit_config.json", "w");
+		wf.write(json_dict);
+		wf.close();
+
+	def get_neighbour_pixel(self, img, x, y, pattern_clr):
+		if x - 1 >= 0 and y - 1 >= 0:
+			cur_clr = img.get_at((x - 1, y - 1));
+			if cur_clr == pattern_clr:
+				return (-1, -1);
+
+		if x - 1 >= 0 and y + 1 < img.get_height():
+			cur_clr = img.get_at((x - 1, y + 1));
+			if cur_clr == pattern_clr:
+				return (-1, 1);
+
+		if x + 1 < img.get_width() and y - 1 >= 0:
+			cur_clr = img.get_at((x + 1, y - 1));
+			if cur_clr == pattern_clr:
+				return (1, -1);
+
+		if x + 1 < img.get_width() and y + 1 < img.get_width():
+			cur_clr = img.get_at((x + 1, y + 1));
+			if cur_clr == pattern_clr:
+				return (1, 1);
+
+		if x - 1 >= 0:
+			cur_clr = img.get_at((x - 1, y));
+			if cur_clr == pattern_clr:
+				return (-1, 0);
+
+		if x + 1 < img.get_width():
+			cur_clr = img.get_at((x + 1, y));
+			if cur_clr == pattern_clr:
+				return (1, 0);
+
+		if y - 1 >= 0:
+			cur_clr = img.get_at((x, y - 1));
+			if cur_clr == pattern_clr:
+				return (0, -1);
+
+		if y + 1 < img.get_height():
+			cur_clr = img.get_at((x, y + 1));
+			if cur_clr == pattern_clr:
+				return (0, 1);
+
+		print("failed");
+		return None;
+
 	def update_input_process(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				exit();
+
 	def update(self):
 		while True:
 			self.update_input_process();
 			self.screen.fill((0, 0, 0));
 			self.screen.blit(self.test_img, (0, 0));
 			pygame.display.update();
-			
-
-
 
 if __name__ == "__main__":
 	converter = Converter("units");
